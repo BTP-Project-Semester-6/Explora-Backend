0a5b6b20f748301118b524aa81b45373d6695067
vijayjoshi16 <joshivijay324@gmail.com>
2022-01-01 11:43:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// DECLARING MODULES
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const env = require("dotenv");
+const http = require("http");
+const bodyParser = require("body-parser");
+
+//DEFINING MODULES
+const app = express();
+const port = 3001 || process.env.PORT;
+const hostname = "localhost";
+
+//VALIDATING MODULES POLICY
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+env.config();
+
+//CONNECTING MONGO DB
+const url = process.env.MONGO_URI;
+
+mongoose.connect(`${url}`);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+//STARTING APP
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server Running at http://${hostname}:${port}/`);
+  });

0b827aaf5c391c6cc22e9f09161532fbefd6830a
vijayjoshi16 <joshivijay324@gmail.com>
2022-01-02 23:07:51 +0530

    Add register user

---
+++ controllers/user.controller.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+exports.addUser = (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    username: req.body.username,
+    picUrl: req.body.picUrl,
+    age: req.body.age,
+    gender: req.body.gender,
+    password: bcrypt.hashSync(req.body.password, 9),
+    email: req.body.email,
+    posts: [],
+    travelHistory: [],
+    badges: [],
+    telegram: req.body.telegram,
+    instagram: req.body.instagram,
+    friends: [],
+  });
+
+  newUser.save((err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: "Adding User Failed" });
+    }
+    return res.status(200).json({
+      user: {
+        _id: data._id,
+        name: data.name,
+        username: data.username,
+      },
+    });
+  });
+};

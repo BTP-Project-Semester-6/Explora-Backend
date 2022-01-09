6809faa26b55fe5bbd2dbf043f5048fbb83e4afb
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-01-09 12:25:27 +0530

    task router created

---
+++ controllers/task.js
@@ -0,0 +1,44 @@
+const challenge = require("../models/challenge");
+const Task = require("../models/Task");
+
+exports.addTask = async (req, res) => {
+  Task.findOne(
+    { userId: req.body.userId, challengeID: req.body.challengeID },
+    function (err, result) {
+      if (err) {
+        return res.status(400).send({ message: "Something Went Wrong" });
+      } else if (!result) {
+        const challengeData = await challenge.findOne({
+          _id: req.body.challengeID,
+        });
+        const newLocations = challengeData.locations.map((item) => {
+          const loc = {};
+          loc["name"] = item.name;
+          loc["lat"] = item.lat;
+          loc["lng"] = item.lng;
+          loc["competed"] = false;
+          return loc;
+        });
+        const newTask = new Task({
+          userId: req.body.userId,
+          challengeID: req.body.challengeID,
+          locations: newLocations,
+          competed: false,
+        });
+        newTask.save((error, data) => {
+          if (error) {
+            console.log(error);
+            return res.status(400).json({ error: "Adding Task Failed" });
+          }
+          if (data) {
+            return res.status(200).json({
+              message: "Success",
+            });
+          }
+        });
+      } else {
+        return res.status(400).send({ message: "Task already active!!!" });
+      }
+    }
+  );
+};

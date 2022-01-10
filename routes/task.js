6f164c7059ad87f8c06580019256edf9d725419a
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-01-10 12:55:50 +0530

    complete sub location in task completed

--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { addTask, getStatusTask } = require("../controllers/task");
+const {
+  addTask,
+  getStatusTask,
+  completeSubLocationInTask,
+} = require("../controllers/task");
 const { addTaskValidate, isTaskValidated } = require("../validator/task");
 
 const router = express.Router();
 
 router.post("/addTask", addTaskValidate, isTaskValidated, addTask);
 router.get("/getStatusTask/:id", getStatusTask);
+router.post("/completeSubLocationInTask", completeSubLocationInTask);
 
 module.exports = router;

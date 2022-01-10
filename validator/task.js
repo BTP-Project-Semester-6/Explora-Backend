cab12ddb5e6a396727862bb962b1af54778ba337
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-01-10 12:59:04 +0530

    complete sublocation in task validator created

--- validator/task.js
+++ validator/task.js
@@ -6,6 +6,13 @@ exports.addTaskValidate = [
   check("userId").notEmpty().withMessage("Please give user id"),
   check("challengeID").notEmpty().withMessage("Please give challenge id"),
 ];
+
+exports.completeSubLocationInTaskValidate = [
+  check("userId").notEmpty().withMessage("Please give user id"),
+  check("taskId").notEmpty().withMessage("Please give challenge id"),
+  check("subLocation").notEmpty().withMessage("Please give sublocation"),
+];
+
 exports.isTaskValidated = (req, res, next) => {
   const errors = validationResult(req);
 

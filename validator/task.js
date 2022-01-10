54fada97e9585f57364e94c6392b3b67ee9db0fb
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-01-10 11:20:40 +0530

    addTask validator created

---
+++ validator/task.js
@@ -0,0 +1,17 @@
+const { check, validationResult } = require("express-validator");
+const Task = require("../models/Task");
+const User = require("../models/User");
+
+exports.addTaskValidate = [
+  check("userTd").notEmpty().withMessage("Please give user id"),
+  check("challengeID").notEmpty().withMessage("Please give challenge id"),
+];
+exports.isTaskValidated = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()[0].msg });
+  }
+
+  next();
+};

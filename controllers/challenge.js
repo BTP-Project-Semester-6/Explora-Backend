d56610ee0501ef63697821f28ad29a8976308a5e
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-02-02 11:13:19 +0530

    getAdminAllNotValidCity, validateChallenge,removeChallenge controllers created

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -29,11 +29,47 @@ exports.newChallenge = (req, res) => {
 exports.getChallengeByCity = (req, res) => {
   console.log(req.body);
   const city = req.body.city;
-  Challenge.find({ city: city })
+  Challenge.find({ city: city, isvalid: true })
     .then((_challenge) => {
       return res.status(200).json(_challenge);
     })
     .catch((err) => {
       return res.status(440).json({ error: "Something went wrong" });
+    });
+};
+
+exports.getAdminAllNotValidCity = (req, res) => {
+  Challenge.find({ isvalid: false })
+    .then((_challenge) => {
+      return res.status(200).json(_challenge);
+    })
+    .catch((err) => {
+      return res.status(440).json({ error: "Something went wrong" });
+    });
+};
+
+exports.validateChallenge = (req, res) => {
+  const id = req.body.id;
+  Challenge.findByIdAndUpdate(id, { $set: { isvalid: true } })
+    .then((data) => {
+      return res.status(200).json({
+        message: "Success",
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: "Something went wrong" });
+    });
+};
+
+exports.removeChallenge = (req, res) => {
+  const id = req.body.id;
+  Challenge.findByIdAndRemove(id)
+    .then((data) => {
+      return res.status(200).json({
+        message: "Success",
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: "Something went wrong" });
     });
 };
